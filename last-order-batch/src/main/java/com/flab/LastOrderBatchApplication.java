e45a91f07f0bf40e3e65cf659804944ffbd0f0c4
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2022-11-13 16:57:14 +0900

    💚 멀티 모듈 프로젝트 적용 (#1)
    * init
    
    * 멀티 모듈 적용 및 test
    
    * Create pull_request_template.md
    
    * PULL_REQUEST_TEMPLATE.md
    
    * batch 추가 및 수정
    
    * ci : gradle 수정 및 gitignore 추가
    
    * build : gradle 수정

---
+++ last-order-batch/src/main/java/com/flab/LastOrderBatchApplication.java
@@ -0,0 +1,11 @@
+package com.flab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastOrderBatchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LastOrderBatchApplication.class);
+    }
+}
---
+++ last-order-core/src/main/java/com/flab/service/TestService.java
@@ -0,0 +1,10 @@
+package com.flab.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+    public String test(){
+        return "core api 연결 확인 test";
+    }
+}
