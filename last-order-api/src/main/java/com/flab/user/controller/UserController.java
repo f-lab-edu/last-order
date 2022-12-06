f662fe008fc429c3a0895e5343b45868bee0f294
유지웅 <poujis0@gmail.com>
2022-12-06 18:00:35 +0900

    feat : Controller 변경 및 loginDTO 생성

--- last-order-api/src/main/java/com/flab/user/controller/UserController.java
+++ last-order-api/src/main/java/com/flab/user/controller/UserController.java
@@ -1,40 +1,34 @@
 package com.flab.user.controller;
 
 import com.flab.user.domain.User;
+import com.flab.user.dto.LoginRequest;
+import com.flab.user.service.UserService;
 import lombok.*;
 import org.springframework.web.bind.annotation.*;
 
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    private final UserService userService;
+
     @PostMapping("/login")
-    public LoginDTO login(@RequestBody LoginDTO login) {
+    public LoginRequest login(@RequestBody LoginRequest login) {
+        userService.login(login);
         return login;
     }
 
     @PostMapping("/signup")
     public User signUp(@RequestBody User user) {
+        userService.signUp(user);
         return user;
     }
-
-    @GetMapping("/duplicate/{email}")
-    public Boolean checkDuplicate(@PathVariable String email) {
-        User user = new User("flab@google.com", "ji",28);
-
-        return user.getEmail().equals(email);
-    }
-
     @GetMapping("/logout")
     public String logout() {
         return "logout";
     }
 
-    @Getter
-    @Setter
-    private static class LoginDTO {
-        @NonNull
-        private String id;
-        @NonNull
-        private String pw;
-    }
 }
+
