b27f3bc1d7a50927219056023f49659e6cef7140
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2022-12-12 20:21:17 +0900

    refactor : exception

--- last-order-api/src/main/java/com/flab/item/exception/ItemNotExistException.java
+++ last-order-api/src/main/java/com/flab/item/exception/ItemNotExistException.java
@@ -3,7 +3,4 @@ package com.flab.item.exception;
 import static com.flab.common.ErrorMessage.*;
 
 public class ItemNotExistException extends RuntimeException{
-    public ItemNotExistException(){
-        super(ITEM_NOT_EXIST.getMessage());
-    }
 }
