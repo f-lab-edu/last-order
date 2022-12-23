471b2c0e5db6b9f7ad84cbf58fc9d3fd18200f6c
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2022-12-23 20:23:48 +0900

    feat : 예외처리

---
+++ last-order-api/src/main/java/com/flab/favorite/exception/AlreadyFavoriteException.java
@@ -0,0 +1,4 @@
+package com.flab.favorite.exception;
+
+public class AlreadyFavoriteException extends RuntimeException{
+}
---
+++ last-order-api/src/main/java/com/flab/store/exception/StoreNotExistException.java
@@ -0,0 +1,4 @@
+package com.flab.store.exception;
+
+public class StoreNotExistException extends RuntimeException{
+}
