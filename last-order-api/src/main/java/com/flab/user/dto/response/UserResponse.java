36427b7cd8dfe5428b864707bd291fce35974983
유지웅 <poujis0@gmail.com>
2023-01-03 19:15:19 +0900

    feat : security 구현

--- last-order-api/src/main/java/com/flab/user/dto/response/UserResponse.java
+++ last-order-api/src/main/java/com/flab/user/dto/response/UserResponse.java
@@ -1,10 +1,14 @@
 package com.flab.user.dto.response;
 
+import com.flab.config.jwt.Authority;
 import com.flab.user.domain.User;
 import com.flab.user.domain.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @AllArgsConstructor
 @Getter
@@ -13,7 +17,7 @@ public class UserResponse {
     private String email;
     private String name;
     private Integer age;
-    private Role role;
+    private Set<Authority> authorities = new HashSet<>();
 
     public static UserResponse from(User user){
         return new UserResponse(
@@ -21,7 +25,7 @@ public class UserResponse {
                 user.getEmail(),
                 user.getName(),
                 user.getAge(),
-                user.getRole()
+                user.getAuthorities()
         );
     }
 }
